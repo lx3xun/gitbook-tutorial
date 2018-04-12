0c4646cc8ae7155426006d000d3b1c1d994e839c
natural <afterthepeak@163.com>
2018-04-12 10:47:18 +0800

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+if [ "$TRAVIS_BRANCH" != "master" ]
+then
+  echo "This commit was made against the $TRAVIS_BRANCH and not the master! No deploy!"
+  exit 0
+fi
+
+rev=$(git rev-parse --short HEAD)
+
+cd stage/_book
+
+git init
+git config user.name "zxfOliver"
+git config user.email "afterthepeak@163.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/zxfOliver/gitbook-tutorial"
+git fetch upstream
+git reset upstream/gh-pages
+
+echo "zxfOliver.com" > CNAME
+
+touch .
+
+git add -A .
+git commit -m "rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
